0edabe3b808c0fc133459baf82d63dfc9aff4427
salabam <pavelgrom.chaban@gmail.com>
2026-07-04 22:50:00 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="text-center fill-height justify-center">
+        <div>
+          <v-icon icon="mdi-image-edit-outline" size="large" color="primary" class="mb-4"></v-icon>
+          <h1 class="text-h4 font-weight-bold mb-2">Image Editor SPA</h1>
+          <p class="text-subtitle-1 text-medium-emphasis mb-6">Vue 3 + Vuetify + Pinia + TS</p>
+          <v-btn color="primary" prepend-icon="mdi-upload">
+            Ready to Code
+          </v-btn>
+        </div>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup ts>
+// Тут пізніше розгорнемо логіку
+</script>
